f93dd97a715233077e55dc5cdbc43ce243db6744
Sajjad Mohammadi <radhoosh.user2@gmail.com>
2018-11-10 14:10:49 +0330

    add Container in index

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,8 +1,10 @@
+import Container from "./Container";
 import ThemeProvider from "./ThemeProvider";
 import Theme from "./ThemeProvider/interface.index";
 import Text from "./Text";
 
 export {
+    Container,
     ThemeProvider,
     Theme,
     Text,
