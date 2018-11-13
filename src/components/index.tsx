b9c3552cfc7ed11e0f4bcad59535d3c2cc395234
Sajjad Mohammadi <radhoosh.user2@gmail.com>
2018-11-13 08:51:04 +0330

    change import grid in index

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,5 @@
+
+export { Grid, Col, Row } from "react-native-easy-grid";
 import StatusBar from "./StatusBar";
 import Right from "./Right";
 import Left from "./Left";
@@ -9,7 +11,7 @@ import Content from "./Content";
 import ThemeProvider from "./ThemeProvider";
 import Theme from "./ThemeProvider/interface.index";
 import Text from "./Text";
-export { Grid, Col, Row } from "react-native-easy-grid";
+
 export {
     StatusBar,
     Right,
