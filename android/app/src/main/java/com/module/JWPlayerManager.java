3172e8ae6c1113684834d72e3944a927d039c8e9
Sajjad Mohammadi <radhoosh.user2@gmail.com>
2019-02-13 09:22:19 +0330

    bug fix in android

---
+++ android/app/src/main/java/com/module/JWPlayerManager.java
@@ -0,0 +1,94 @@
+package com.module;
+
+
+import com.facebook.react.uimanager.SimpleViewManager;
+import com.facebook.react.uimanager.ThemedReactContext;
+import com.facebook.react.uimanager.annotations.ReactProp;
+import com.longtailvideo.jwplayer.media.playlists.PlaylistItem;
+
+import javax.annotation.Nullable;
+
+public class JWPlayerManager extends SimpleViewManager<JWView> {
+    JWView jwview;
+    ThemedReactContext context;
+    PlaylistItem pi;
+    String uri,title,description;
+    @Override
+    public String getName() {
+        return "JWPlayer";
+    }
+
+    @Override
+    protected JWView createViewInstance(ThemedReactContext reactContext) {
+         context = reactContext;
+//         PlayerConfig playerConfig = new PlayerConfig.Builder().build();
+         jwview= new JWView(reactContext);
+//         pi= new PlaylistItem.Builder()
+//                    .file("http://playertest.longtailvideo.com/adaptive/bipbop/gear4/prog_index.m3u8")
+//                    .title("BipBop")
+//                    .description("A  player testing video.")
+//                    .build();
+//         jwview.load(pi);
+         return jwview;
+//        return new View(context);
+    }
+//
+    @ReactProp(name= "src")
+    public  void setSrc(JWView v,@Nullable String src){
+        if( src!= null) {
+            pi= new PlaylistItem.Builder()
+                    .file(src)
+                    .title(title!= null ? title:"")
+                    .description(description!=null?description:" ")
+                    .build();
+            jwview.load(pi);
+        }
+    }
+//
+    @ReactProp(name= "title")
+    public  void setTitle(JWView v,@Nullable String title){
+             this.title=title;
+    }
+
+    @ReactProp(name= "description")
+    public  void setDescription(JWView v,@Nullable String description){
+             this.description=description;
+    }
+
+    @ReactProp(name= "play")
+    public  void setPlay(JWView v, Boolean play){
+
+        if(play){
+            if(this.uri!= null){
+//                if(this.title!= null){
+//                    if(this.description!= null){
+//                        pi= new PlaylistItem.Builder()
+//                                .file("http://playertest.longtailvideo.com/adaptive/bipbop/gear4/prog_index.m3u8")
+//                                .title("BipBop")
+//                                .description("A player testing video.")
+//                                .build();
+//                v.load(pi);
+//                    }
+//                }
+            }
+
+        }
+
+    }
+
+//    @ReactProp(name= "player")
+//    public  void setPlayer(JWPlayerView jw,Boolean player){
+
+//        if(start){
+////            pi= new PlaylistItem.Builder()
+////                    .file("http://playertest.longtailvideo.com/adaptive/bipbop/gear4/prog_index.m3u8")
+////                    .title("BipBop")
+////                    .description("A   player testing video.")
+////                    .build();
+////            jw.load(pi);
+//        }
+//
+//    }
+
+
+}
---
+++ android/app/src/main/java/com/module/JWView.java
@@ -0,0 +1,51 @@
+package com.module;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+import com.facebook.react.bridge.ReactContext;
+import com.longtailvideo.jwplayer.JWPlayerView;
+import com.longtailvideo.jwplayer.media.playlists.PlaylistItem;
+
+public class JWView extends FrameLayout {
+
+
+    private final Context _context;
+    private Activity activity = null;
+    JWPlayerView playerView;
+    public JWView(Context context) {
+        super(context);
+        this._context = context;
+        this.activity = ((ReactContext) getContext()).getCurrentActivity();
+        this.playerView = new JWPlayerView(this.activity, (AttributeSet) null );
+        addView(this.playerView);
+    }
+
+    private final Runnable measureAndLayout = new Runnable() {
+        @Override
+        public void run() {
+            measure(
+                    MeasureSpec.makeMeasureSpec(getWidth(), MeasureSpec.EXACTLY),
+                    MeasureSpec.makeMeasureSpec(getHeight(), MeasureSpec.EXACTLY));
+            layout(getLeft(), getTop(), getRight(), getBottom());
+        }
+    };
+
+    @Override
+    public void requestLayout() {
+        super.requestLayout();
+
+        // The spinner relies on a measure + layout pass happening after it calls requestLayout().
+        // Without this, the widget never actually changes the selection and doesn't call the
+        // appropriate listeners. Since we override onLayout in our ViewGroups, a layout pass never
+        // happens after a call to requestLayout, so we simulate one here.
+        post(measureAndLayout);
+    }
+
+
+    public void load(PlaylistItem param) {
+        this.playerView.load( param );
+    }
+}
